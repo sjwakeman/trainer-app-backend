842d909067672b008243f7d6151b7440a12f79a8
sjwakeman <swakeman@ca.rr.com>
2019-09-20 09:13:33 -0700

    before_action authenticate_user only show; Methods home, signin, new, create, index, show, destroy, search, search_submit, private user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,27 +1,70 @@
 class Api::V1::UsersController < ApplicationController
-    before_action :find_user, only: [:update]
-    def index
-      @users = User.all
-      render json: @users
+  before_action :authenticate_user, only: [:show]
+
+  def home
+  end
+
+  def signin
+    @user = User.new
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      # UsersController create logs you in
+        redirect_to user_path(@user)
+        # new server request happens, so the previous controller
+        #instance is destroyed and a new controller instance is created.
+    else
+      flash[:notice] = "Invalid name/password combination."
+      render :new
+      #When you render, you remain in the same controller instance
     end
-    
-    def update
-      @user.update(user_params)
-      if @user.save
-        render json: @user, status: :accepted
-      else
-        render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    if params[:message]
+      @message = params[:message] #Displays message
+    else
+      @message = "" #If no message prevents Error
+    end
+      if current_user != @user
+        redirect_to root_path
       end
-    end
-    
-    private
-    
-    def user_params
-      params.require(:user).permit(:name, :password, :email, :image, :uid)
-    end
-    
-    def find_user
-      @user = User.find(params[:id])
-    end
+  end
+
+  def destroy
+    user.destroy
+    flash[:message] = "You have successfully logged out."
+    redirect_to '/'
+  end
+
+  def search
+    @users=User.all
+  end
+
+
+  def search_submit
+      #Displays User Name search 
+      @users=User.all
+      @users=@users.search(params[:search])
+        render :search
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :email, :image, :uid)
+  end
 
 end
