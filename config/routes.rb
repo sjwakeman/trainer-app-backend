3731050f266db2046e082164d91adeedecacbfb3
sjwakeman <swakeman@ca.rr.com>
2019-09-19 20:18:32 -0700

    namespace api, namespace v1, resources users and resources training_sessions for only index and update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :update]
+      resources :training_sessions, only: [:index, :update]
+    end
+  end
 end
